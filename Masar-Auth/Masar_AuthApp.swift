01d060f8a08cfed2c615e9ca065768690e2bfc27
Raneem AlRashoud <439200695@student.ksu.edu.sa>
2022-07-15 18:08:43 +0300

    Initial Commit

---
+++ Masar-Auth/Masar_AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Masar_AuthApp.swift
+//  Masar-Auth
+//
+//  Created by Raneem AlRashoud on 16/12/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct Masar_AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
